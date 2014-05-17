4f6171f04cd9710dd9555209bf64e775fd8cb688
roshult <roshult89@gmail.com>
2014-05-17 18:08:55 +0200

    Äntligen fungerar det, skriver dock ut level som namn!

--- Prutt_lab5/src/Tree.java
+++ Prutt_lab5/src/Tree.java
@@ -23,46 +23,49 @@ public class Tree extends TreeFrame{
     void initTree(){
     	root = new DefaultMutableTreeNode(noder.get(0).level);
     	String startLevel = noder.get(0).level;
-    	String endLevel = "/"+noder.get(0).level;
-    	while(position<noder.size()){
-    		addChild(noder.get(position),root, startLevel, endLevel);
+    	String endLevel = "";
+    	while(! endLevel.equals("/"+startLevel)){
     		position++;
+    		if(noder.get(position).level.startsWith("/")){
+    			System.out.println("----");
+    			endLevel = noder.get(position).level;
+    		}
+    		else{
+    			endLevel = noder.get(position).level;	
+    			addChild(noder.get(position),root);
+    		}
     	}
 		treeModel = new DefaultTreeModel(root);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tree = new JTree( treeModel );
 	}
     
-    public void addChild(MyNode nod,DefaultMutableTreeNode parent, String startLevel, String endLevel){
-    	if (! nod.level.equals(startLevel) && ! nod.level.equals(endLevel) &&! nod.level.substring(0,1).equals("/")){
+    public void addChild(MyNode nod,DefaultMutableTreeNode parent){
     		DefaultMutableTreeNode child = new DefaultMutableTreeNode(nod.level);
 			parent.add(child);
-			
+			System.out.println(nod.level);
     		if(ifDic(nod)){
-    			String startlevel =nod.level;
-    			String endlevel = ""; 
-    			while(! endlevel.equals("/"+startlevel)){
-    				
-    				endlevel = "/"+noder.get(position).level;
-    				
-    				position++;
-    				System.out.println("Start--->"+startlevel);
-    				System.out.println("END--->"+endlevel);
-    				
-    				addChild(noder.get(position),child,"","");
-    				
-    			}
-    		
+    			String startLevel =nod.level;
+    			String endLevel = "";
+    			while(! endLevel.equals("/"+startLevel)){
+    	    		position++;
+    	    		if(noder.get(position).level.startsWith("/")){
+    	    			System.out.println("----");
+    	    			endLevel = noder.get(position).level;
+    	    		}
+    	    		else{
+    	    			endLevel = noder.get(position).level;	
+    	    			addChild(noder.get(position),child);
+    	    		}
+    	    	}
     		}
     	}
-    }
     	  
     
     public boolean ifDic(MyNode nod){
-    	System.out.println("1 = "+(noder.get(position).level) );
-    	System.out.println("2 = "+(noder.get(position+1).level) );
-		return (! ("/"+nod.level).equals(noder.get(position+1).level));
-    }
+        	return (! ("/"+nod.level).equals(noder.get(position+1).level) && ! nod.level.startsWith("/"));
+  }
+    
     
     public ArrayList<MyNode> getInside(){
     	inside = new ArrayList<MyNode>();
