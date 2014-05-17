2d20bc4a12241349726019bba0822a7111f6b9d0
roshult <roshult89@gmail.com>
2014-05-17 18:41:26 +0200

    Klar med rekursivt träd som skriver ut mappnamnen med svenska tecken

--- Prutt_lab5/src/MyNode.java
+++ Prutt_lab5/src/MyNode.java
@@ -2,15 +2,27 @@
 public class MyNode {
 	
 	public String level;
+	public String name;
 
 	
 	MyNode(String str){
 	  level = getLevel(str);
+	  name = getName(str);
 		
 	}
 	private String getLevel(String str){
 		String s = str.split(" ")[0].replace("<", "").replace(">", "");
 		return s;
+	}
+	private String getName(String str){
+		String[] s = str.split("\"");
+		if (s.length>1){
+			String name = s[1];
+			return name;
+		}
+		else{
+			return null;
+		}
 	}
 
 }
--- Prutt_lab5/src/Tree.java
+++ Prutt_lab5/src/Tree.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -21,13 +23,12 @@ public class Tree extends TreeFrame{
 	static int i=0;
 	
     void initTree(){
-    	root = new DefaultMutableTreeNode(noder.get(0).level);
+    	root = new DefaultMutableTreeNode(noder.get(0).name);
     	String startLevel = noder.get(0).level;
     	String endLevel = "";
     	while(! endLevel.equals("/"+startLevel)){
     		position++;
     		if(noder.get(position).level.startsWith("/")){
-    			System.out.println("----");
     			endLevel = noder.get(position).level;
     		}
     		else{
@@ -41,16 +42,14 @@ public class Tree extends TreeFrame{
 	}
     
     public void addChild(MyNode nod,DefaultMutableTreeNode parent){
-    		DefaultMutableTreeNode child = new DefaultMutableTreeNode(nod.level);
+    		DefaultMutableTreeNode child = new DefaultMutableTreeNode(nod.name);
 			parent.add(child);
-			System.out.println(nod.level);
     		if(ifDic(nod)){
     			String startLevel =nod.level;
     			String endLevel = "";
     			while(! endLevel.equals("/"+startLevel)){
     	    		position++;
     	    		if(noder.get(position).level.startsWith("/")){
-    	    			System.out.println("----");
     	    			endLevel = noder.get(position).level;
     	    		}
     	    		else{
@@ -65,33 +64,21 @@ public class Tree extends TreeFrame{
     public boolean ifDic(MyNode nod){
         	return (! ("/"+nod.level).equals(noder.get(position+1).level) && ! nod.level.startsWith("/"));
   }
-    
-    
-    public ArrayList<MyNode> getInside(){
-    	inside = new ArrayList<MyNode>();
-    	int i = position;
-    	String level = "";
-    	while (! level.equals("/"+noder.get(position).level)){
-    		level = noder.get(i+1).level;
-    		inside.add(noder.get(i));
-    		i++;	
-    	}
-		return inside;
-    }
+ 
     
     public static void main(String[] args) {
     	list = new ArrayList<String>();
     	noder = new ArrayList<MyNode>();
     	BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader("Liv.txt"));
+			br = new BufferedReader(new InputStreamReader(new FileInputStream("Liv.txt"), "ISO-8859-1"));
 		     String line = br.readLine();
 		    
 		     while (line != null) {
 		            list.add(line);
 		            //System.out.println(line);
 		            line = br.readLine();
-		        }
+		     }
 		     br.close();
 		    
 		} catch (FileNotFoundException e1) {
@@ -105,6 +92,7 @@ public class Tree extends TreeFrame{
 		for (String str:list){
 			noder.add(new MyNode(str));
 		}
+				
     	new Tree();
         }
 }
